90bf7e09d3346aef0b1dda93d495324fa4983345
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2020-09-18 12:18:06 -0400

    delete

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
+<script src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+  <script src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+  <script src="https://unpkg.com/babel-standalone@6.15.0/babel.min.js"></script>
 
 //JSX javascript extension
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'))
 
