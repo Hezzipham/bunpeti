3d099751c814c065e87010631497ea771853e300
Hezzipham <52872927+Hezzipham@users.noreply.github.com>
2021-07-05 10:54:25 -0400

    vc

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,6 @@
-console.log("hello")
+var contactForm = document.querySelector('#contact');
+contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert("Message sent!");
+    document.getElementById("contact").reset();
+});
